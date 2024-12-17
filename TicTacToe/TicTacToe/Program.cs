33a08c60e5262515471f1212e4199641eeb2d3e1
Andreas Röst <andreas.rost@winlas.se>
2024-12-17 09:29:24 +0100

    feat: added readme file

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -1,6 +1,31 @@
 ﻿using TicTacToe;
 
-var game = new Game(4);
-game.Run();
+//var game = new Game(3);
+//game.Run();
 
+var result = MakeComplement("ATTGC");
+Console.WriteLine(result);
 
+ static string MakeComplement(string dna)
+{
+    string result = "";
+    foreach (var c in dna)
+    {
+        switch (c)
+        {
+            case 'G':
+                result += 'C';
+                break;
+            case 'C':
+                result += 'G';
+                break;
+            case 'A':
+                result += 'T'; 
+                break;
+            case 'T':
+                result += 'A';
+                break;
+        }
+    }
+    return result;
+}
